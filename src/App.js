4a0e39e0616e6dd3b6ad34df7ae9b9ba60cce4ec
JisuEun <zkxktmxmfhvl@gmail.com>
2024-05-24 02:16:01 +0900

    page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import StartPage from './pages/StartPage';
 import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import SelectPage from './pages/SelectPage';
+import CustomerMainPage from './pages/CustomerMainPage';
+import OwnerMainPage from './pages/OwnerMainPage';
 
 function App() {
   return (
@@ -9,6 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<StartPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/customer/main" elememt={<CustomerMainPage/>}/>
+        <Route path="/signup" elements={<SignUpPage/>}/>
+        <Route path="/signup/select" elements={<SelectPage/>}/>
+        <Route path="/owner/main" elements={<OwnerMainPage/>}/>
       </Routes>
     </Router>
   );
