89b63808a0a37aeafd0040477991925a481190b0
JisuEun <zkxktmxmfhvl@gmail.com>
2024-05-24 02:19:00 +0900

    startpage - link added

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function StartPage() {
   return (
     <div>
       <h1>Start Page</h1>
+      <p>임시 페이지</p>
+      <Link to="/login">로그인</Link>
+      <Link to="/signup">회원가입</Link>
+      <Link to="/select">선택 화면</Link>
+      <Link to="/customer/main">고객 메인</Link>
+      <Link to="/owner/main">사장 메인</Link>
     </div>
   );
 }
