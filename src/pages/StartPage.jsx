35451e0054ec04547e3d16f83e4a6ab38199b569
JisuEun <zkxktmxmfhvl@gmail.com>
2024-05-24 02:22:37 +0900

    div added

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -6,11 +6,11 @@ function StartPage() {
     <div>
       <h1>Start Page</h1>
       <p>임시 페이지</p>
-      <Link to="/login">로그인</Link>
-      <Link to="/signup">회원가입</Link>
-      <Link to="/select">선택 화면</Link>
-      <Link to="/customer/main">고객 메인</Link>
-      <Link to="/owner/main">사장 메인</Link>
+      <div><Link to="/login">로그인</Link></div>
+      <div><Link to="/signup">회원가입</Link></div>
+      <div><Link to="/select">선택 화면</Link></div>
+      <div><Link to="/customer/main">고객 메인</Link></div>
+      <div><Link to="/owner/main">사장 메인</Link></div>
     </div>
   );
 }
