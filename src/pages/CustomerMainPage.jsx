0fea55bd322a35a449ecdc42a4ea4910296cca34
song <74825547+momo0522@users.noreply.github.com>
2024-06-09 16:51:53 +0900

    CustomerMainPage style 개선

--- src/pages/CustomerMainPage.jsx
+++ src/pages/CustomerMainPage.jsx
@@ -1,32 +1,50 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,useLocation } from 'react-router-dom';
 import '../styles/CustomerMainPage.css';
-import sampleImage from '../assets/sample.png';
+import sampleImage from '../assets/user.png';
+import profileImage from '../assets/user.png';
+import couponlistImage from '../assets/couponlist.png';
 
 const CustomerMainPage = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const nickname = location.state?.nickname || '고객';
+  
+  const handleModifyCustomerClick = () => {
+    navigate('/customer/profile/modify');
+  };
 
-  const handleRedirect = () => {
-    navigate('/');
+  const handleCouponListClick = () => {
+    navigate('/customer/coupon-box');
+  };
+
+  const handleLoginClick = () => {
+    navigate('/login');
+  };
+
+  const handleClick = () => {
+    navigate('/customer/main');
   };
 
   return (
     <div className="customer-main-page">
       <div className="header">
-        <img src={sampleImage} alt="Profile" className="profile-icon" onClick={handleRedirect} />
-        <img src={sampleImage} alt="Notifications" className="notification-icon" onClick={handleRedirect} />
-      </div>
+        <img src={profileImage} alt="Profile" className="profile-icon" onClick={handleModifyCustomerClick} />
+        <h1 className="title" onClick={handleClick}>쿠폰모아</h1>
+        <img src={couponlistImage} alt="coupinlist" className="couponlist-icon" onClick={handleCouponListClick} />
+      </div> 
+      
       <div className="content">
         <div className="avatar">
           <img src={sampleImage} alt="Avatar" />
         </div>
         <div className="welcome-text">
-          <span className="highlight" onClick={handleRedirect}>안녕</span> 고객님,
+          <span className="highlight" onClick={handleModifyCustomerClick}>{nickname}</span> 고객님,
           <br />
           안녕하세요
         </div>
         <div className="logout-container">
-          <span className="logout-button" onClick={handleRedirect}>로그아웃</span>
+          <span className="logout-button" onClick={handleLoginClick}>로그아웃</span>
         </div>
       </div>
     </div>
