e044919c7956c9101ec2a2f20dba3c86b5ad0eaa
song <74825547+momo0522@users.noreply.github.com>
2024-06-09 16:52:31 +0900

    CustomerMapPage style 개선

--- src/pages/CustomerMapPage.jsx
+++ src/pages/CustomerMapPage.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import KakaoMap from "../components/KakaoMap";
+import backImage from '../assets/back.png';
+
+
 
 function CustomerMapPage() {
+  const navigate = useNavigate();
+
+  const handleCouponClick = () => {
+    navigate('/customer/coupon-box');
+  }
+
+
   return (
-    <div>
-      <h1>카카오 지도</h1>
-      <KakaoMap />
+    <div className="map-container">
+      <header className="header">
+      <div className="back-icon" onClick={handleCouponClick}>
+          <img src={backImage} alt="Back Icon" />
+        </div>
+        <h1 className="title">카카오 지도</h1>
+      </header>
+      <div className="map-content">
+        <KakaoMap />
+      </div>
     </div>
   );
 }
