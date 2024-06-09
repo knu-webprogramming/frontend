92289910d06f43f496a1cd1321d8feab56f11dec
song <74825547+momo0522@users.noreply.github.com>
2024-06-09 16:54:26 +0900

    OwnerMainPage style 개선

--- src/pages/OwnerMainPage.jsx
+++ src/pages/OwnerMainPage.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/OwnerMainPage.css';
-import sampleImage from '../assets/sample.png';
+import logoexImage from '../assets/cafelogo.png';
+import profileImage from '../assets/user.png';
+import notificationImage from '../assets/mainnotification.png';
+
 
 const OwnerMainPage = () => {
   const navigate = useNavigate();
@@ -10,15 +13,27 @@ const OwnerMainPage = () => {
     navigate('/');
   };
 
+  const handleModifyOwnerClick = () => {
+    navigate('/owner/profile/modify');
+  };
+
+  const handleApprovalClick = () => {
+    navigate('/owner/approval');
+  };
+
+  const handleLoginClick = () => {
+    navigate('/login');
+  };
+
   return (
     <div className="owner-main-page">
       <div className="header">
-        <img src={sampleImage} alt="Profile" className="profile-icon" onClick={handleRedirect} />
-        <img src={sampleImage} alt="Notifications" className="notification-icon" onClick={handleRedirect} />
+        <img src={profileImage} alt="Profile" className="profile-icon" onClick={handleModifyOwnerClick} />
+        <img src={notificationImage} alt="Notifications" className="notification-icon" onClick={handleApprovalClick} />
       </div>
       <div className="content">
         <div className="avatar">
-          <img src={sampleImage} alt="Avatar" />
+          <img src={logoexImage} alt="Avatar" />
         </div>
         <div className="welcome-text">
           <span className="highlight">바바리안</span> 점주님,
@@ -26,7 +41,7 @@ const OwnerMainPage = () => {
           안녕하세요
         </div>
         <div className="logout-container">
-          <span className="logout-button" onClick={handleRedirect}>로그아웃</span>
+          <span className="logout-button" onClick={handleLoginClick}>로그아웃</span>
         </div>
       </div>
     </div>
