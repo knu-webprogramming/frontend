d07341122f3fcb9433c1fc2194b6ab698a2760da
song <74825547+momo0522@users.noreply.github.com>
2024-06-09 16:53:28 +0900

    CustomerProfilePage style 개선

--- src/pages/CustomerProfilePage.jsx
+++ src/pages/CustomerProfilePage.jsx
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/CustomerProfilePage.css';
-import defaultProfileImage from '../assets/sample.png';
+import defaultProfileImage from '../assets/profileimage.png';
+
 
 function CustomerProfilePage() {
   const [profileImage, setProfileImage] = useState(null);
+  const [nickname,setNickname]=useState('');
+  const navigate = useNavigate();
 
   const handleImageChange = (e) => {
     if (e.target.files && e.target.files[0]) {
       setProfileImage(URL.createObjectURL(e.target.files[0]));
     }
+  };
+
+  const handleNicknameChange = (e) => {
+    setNickname(e.target.value);
+  };
+
+  const handleCustomerMainClick = () => {
+    navigate('/customer/main', { state: { nickname } });
   };
 
   return (
@@ -31,9 +43,13 @@ function CustomerProfilePage() {
       </div>
       <div className="input-group">
         <label htmlFor="nickname">닉네임</label>
-        <input type="text" id="nickname" placeholder="안뇽" />
+        <input type="text" 
+        id="nickname" 
+        placeholder="사용할 닉네임을 입력하세요" 
+        value={nickname}
+        onChange={handleNicknameChange}/>
       </div>
-      <button className="submit-button">등록</button>
+      <button className="submit-button" onClick={handleCustomerMainClick}>등록</button>
     </div>
   );
 }
