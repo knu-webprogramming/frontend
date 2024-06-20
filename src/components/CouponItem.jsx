39f34c3b5037dec3fb0df4299a082209ed58997b
JisuEun <zkxktmxmfhvl@gmail.com>
2024-06-20 20:21:07 +0900

    CouponItem fixed

--- src/components/CouponItem.jsx
+++ src/components/CouponItem.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import '../styles/CouponItem.css';
 
-const CouponItem = ({ name, profileImageUrl, shopId }) => {
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate(`/customer/coupon/${shopId}`);
-  };
-
+const CouponItem = ({ name, profileImageUrl, shopId, onClick }) => {
   return (
-    <div className="coupon-item" onClick={handleClick}>
+    <div className="coupon-item" onClick={onClick}>
       <img src={profileImageUrl} alt={name} className="coupon-image" />
       <p className="coupon-name">{name}</p>
     </div>
