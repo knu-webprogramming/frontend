3ecbf3894bf51b449e8fd3e0e812c88705a0ae39
JisuEun <zkxktmxmfhvl@gmail.com>
2024-06-21 00:11:23 +0900

    CustomerProfileModifyPage.jsx

--- src/pages/CustomerProfileModifyPage.jsx
+++ src/pages/CustomerProfileModifyPage.jsx
@@ -2,63 +2,60 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
-import '../styles/CustomerProfileModifyPage.css';
+import '../styles/OwnerProfilePage.css';
+import couponaddImage from '../assets/couponadd.png';
+import couponImage from '../assets/coupon.png';
+import defaultProfileImage from '../assets/profileimage.png';
+import KakaoMapProfile from '../components/KakaoMapProfile';
+import glassesImage from '../assets/glasses.png';
 
-function CustomerProfileModifyPage() {
-  const [profileImage, setProfileImage] = useState('');
-  const [image, setImage] = useState(null); // 업로드된 파일을 저장
-  const [name, setName] = useState('');
+const OwnerProfilePage = () => {
   const navigate = useNavigate();
-  const token = useSelector((state) => state.token.token); // Redux에서 토큰을 가져옴
+  const [storeName, setStoreName] = useState('');
+  const [address, setAddress] = useState('');
+  const [ownerName, setOwnerName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [couponBenefit, setCouponBenefit] = useState('');
+  const [stampCount, setStampCount] = useState('10');
+  const [stampType, setStampType] = useState('');
+  const [selectedCoupon, setSelectedCoupon] = useState('');
+  const [isMapOpen, setIsMapOpen] = useState(false);
+  const [profileImage, setProfileImage] = useState(null);
+  const [imageFile, setImageFile] = useState(null);
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const token = useSelector((state) => state.token.token);
 
-  useEffect(() => {
-    const fetchProfileData = async () => {
-      try {
-        const response = await axios.get('https://api.couponmoa.click/customer', {
-          headers: {
-            'Authorization': `Bearer ${token}`,
-          },
-        });
+  const handleRedirect = () => {
+    navigate('');
+  };
 
-        if (response.status === 200) {
-          const data = response.data;
-          setName(data.name);
-          if (data.profileImageUrl) {
-            // 이미지 URL을 절대 경로로 변환
-            setProfileImage(`https://${data.profileImageUrl}`);
-          }
-        }
-      } catch (error) {
-        console.error('Error fetching profile data:', error);
-      }
-    };
-
-    fetchProfileData();
-  }, [token]);
-
-  const handleImageChange = (e) => {
-    if (e.target.files && e.target.files[0]) {
-      setProfileImage(URL.createObjectURL(e.target.files[0]));
-      setImage(e.target.files[0]); // 파일 저장
+  const handleOwnerMainClick = async () => {
+    if (!storeName || !address || !ownerName || !phoneNumber || !couponBenefit || !lat || !lng || !stampType) {
+      alert("모든 정보를 입력한 후 등록해주세요.");
+      return;
     }
-  };
 
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
+    if (!imageFile) {
+      alert("프로필 사진을 추가한 후 등록해주세요.");
+      return;
+    }
 
-  const handleCustomerMainClick = async () => {
     const formData = new FormData();
-    formData.append('name', name);
-
-    if (image) {
-      formData.append('image', image);
-    } else {
-      formData.append('image', null);
-    }
+    formData.append('name', storeName);
+    formData.append('phoneNum', phoneNumber);
+    formData.append('ownerName', ownerName);
+    formData.append('maxStamps', stampCount);
+    formData.append('reward', couponBenefit);
+    formData.append('placeName', storeName);
+    formData.append('addressName', address);
+    formData.append('y', lat);
+    formData.append('x', lng);
+    formData.append('image', imageFile);
+    formData.append('stampType', stampType);
 
     try {
-      const response = await axios.post('https://api.couponmoa.click/customer', formData, {
+      const response = await axios.post('https://api.couponmoa.click/shop', formData, {
         headers: {
           'Authorization': `Bearer ${token}`,
           'Content-Type': 'multipart/form-data',
@@ -69,19 +66,61 @@ function CustomerProfileModifyPage() {
         throw new Error('Network response was not ok');
       }
 
-      navigate('/customer/main', { state: { name } });
+      navigate('/owner/main');
     } catch (error) {
       console.error('There was a problem with the axios operation:', error);
-      if (error.response) {
-        console.error('Response:', error.response.data);
-      }
+      console.error('Response:', error.response);
     }
   };
 
+  const handleMapRedirect = () => {
+    setIsMapOpen(true);
+  };
+
+  const handleImageChange = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      setProfileImage(URL.createObjectURL(e.target.files[0]));
+      setImageFile(e.target.files[0]);
+    }
+  };
+
+  const formatPhoneNumber = (number) => {
+    const cleanNumber = number.replace(/\D/g, '');
+    const match = cleanNumber.match(/^(\d{3})(\d{4})(\d{4})$/);
+    if (match) {
+      return `${match[1]}-${match[2]}-${match[3]}`;
+    }
+    return number;
+  };
+
+  const handlePhoneNumberChange = (e) => {
+    const formattedPhoneNumber = formatPhoneNumber(e.target.value);
+    setPhoneNumber(formattedPhoneNumber);
+  };
+
+  const handlePlaceSelect = (place) => {
+    if (place) {
+      setStoreName(place.name);
+      setAddress(place.address);
+      setLat(place.lat);
+      setLng(place.lng);
+    }
+    setIsMapOpen(false);
+  };
+
+  const closeMap = () => {
+    setIsMapOpen(false);
+  };
+
+  const handleCouponClick = (type) => {
+    setStampType(type);
+    setSelectedCoupon(type);
+  };
+
   return (
-    <div className="form-container">
-      <h1 className="title">회원 정보 수정</h1>
-      <div className="profile-pic-container">
+    <div className="ownerprofile-container">
+      <h1 className="title">가게 정보 입력</h1>
+      <div className="profile-image">
         <input
           type="file"
           id="profileImage"
@@ -89,24 +128,103 @@ function CustomerProfileModifyPage() {
           onChange={handleImageChange}
         />
         <label htmlFor="profileImage" className="profile-pic-label">
-          {profileImage && (
+          {profileImage ? (
             <img src={profileImage} alt="Profile" className="profile-pic" />
+          ) : (
+            <img src={defaultProfileImage} alt="Default Profile" className="profile-pic" />
           )}
         </label>
       </div>
-      <div className="input-group">
-        <label htmlFor="name">닉네임</label>
-        <input
-          type="text"
-          id="name"
-          placeholder="사용할 닉네임을 입력하세요"
-          value={name}
-          onChange={handleNameChange}
-        />
-      </div>
-      <button className="submit-button" onClick={handleCustomerMainClick}>수정</button>
+      <form className="form">
+        <div className="form-group store-name-group input-with-icon">
+          <label>가게명</label>
+          <input
+            type="text"
+            placeholder="가게명을 입력하세요"
+            value={storeName}
+            readOnly
+          />
+          <img
+            src={glassesImage}
+            alt="Search Store"
+            className="glasses-icon"
+            onClick={handleMapRedirect}
+          />
+        </div>
+        <div className="form-group address-group">
+          <label>주소</label>
+          <input
+            type="text"
+            placeholder="주소를 입력하세요"
+            value={address}
+            readOnly
+          />
+        </div>
+        <div className="form-group">
+          <label>사업자 이름</label>
+          <input
+            type="text"
+            placeholder="사업자 이름을 입력하세요"
+            value={ownerName}
+            onChange={(e) => setOwnerName(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label>전화번호</label>
+          <input
+            type="text"
+            placeholder="전화번호를 입력하세요"
+            value={phoneNumber}
+            onChange={handlePhoneNumberChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>쿠폰 모양</label>
+          <div className="coupon-selection">
+            <img
+              src={couponImage}
+              alt="Coupon"
+              onClick={() => handleCouponClick('type1')}
+              className={selectedCoupon === 'type1' ? 'selected' : ''}
+            />
+            <span>or</span>
+            <img
+              src={couponaddImage}
+              alt="Add Coupon"
+              onClick={() => handleCouponClick('type2')}
+              className={selectedCoupon === 'type2' ? 'selected' : ''}
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <label>도장 개수</label>
+          <select value={stampCount} onChange={(e) => setStampCount(e.target.value)}>
+            <option value="10">10개</option>
+            <option value="20">20개</option>
+            <option value="30">30개</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label>쿠폰 혜택</label>
+          <input
+            type="text"
+            placeholder="쿠폰 혜택을 입력하세요"
+            value={couponBenefit}
+            onChange={(e) => setCouponBenefit(e.target.value)}
+          />
+        </div>
+        <button type="button" className="submit-button" onClick={handleOwnerMainClick}>등록</button>
+      </form>
+      {isMapOpen && (
+        <div className="modal" onClick={closeMap}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <span className="close" onClick={closeMap}>&times;</span>
+            <KakaoMapProfile onPlaceSelect={handlePlaceSelect} />
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default CustomerProfileModifyPage;
+export default OwnerProfilePage;
