366d3b3dd580528beb04e14f948ad2433d54701d
JisuEun <zkxktmxmfhvl@gmail.com>
2024-06-24 00:28:37 +0900

    font

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import store, { persistor } from './redux/store';
+import './styles/global.css'
 
 ReactDOM.render(
   <Provider store={store}>
