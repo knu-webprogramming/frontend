c6d0de8162598befd6f1786c17c2ae26dfae8a45
JisuEun <zkxktmxmfhvl@gmail.com>
2024-06-23 23:29:59 +0900

    start

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/StartPage.css';
 import couponImage from "../assets/logo.png";
-import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function StartPage() {
   const navigate = useNavigate();
+  const [countdown, setCountdown] = useState(3);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const timer = setInterval(() => {
+      setCountdown(prevCountdown => prevCountdown - 1);
+    }, 1000);
+
+    const navigateTimeout = setTimeout(() => {
       navigate('/login');
     }, 3000);
 
-    return () => clearTimeout(timer); // 컴포넌트가 언마운트되면 타이머 정리
+    return () => {
+      clearInterval(timer);
+      clearTimeout(navigateTimeout); // 컴포넌트가 언마운트되면 타이머 정리
+    };
   }, [navigate]);
 
   return (
- <div className="start-container">
+    <div className="start-container">
       <img src={couponImage} alt="쿠폰모아" className="start-logo" />
-    <div className="start-text"> This page will redirect to login page in 3 seconds.</div>
+      <div className="start-text"> This page will redirect to login page in {countdown} seconds.</div>
     </div> 
   );
 }
