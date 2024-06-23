c7247ab3cfcef60d35aa69d86b222aa1914d61d2
JisuEun <zkxktmxmfhvl@gmail.com>
2024-06-24 00:43:20 +0900

    font

---
+++ src/GlobalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+import "./fonts/fonts.css";
+
+const GlobalStyle = createGlobalStyle`
+body {
+  font-family: 'Pretendard';
+}
+`
+
+export default GlobalStyle;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import store, { persistor } from './redux/store';
-import './styles/global.css'
+import GlobalStyle from './GlobalStyle';
 
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
+      <GlobalStyle />
       <App />
     </PersistGate>
   </Provider>,
